9025da2b5d457f085960a74f34b717688cfb671e
PauloManao <prg.junior@hotmail.com>
2023-07-25 19:47:55 +0100

    Set up new GUI
    Deleted the previous GUI created on IntelliJ and built a new one on Eclipse as it was difficult to build it on IntelliJ

--- CADistributedSystems/src/main/java/ds/project/GUIApp.java
+++ CADistributedSystems/src/main/java/ds/project/GUIApp.java
@@ -1,14 +1,43 @@
 package ds.project;
 
+import ds.project.service1.Service1Grpc;
+import ds.project.service1.Service1OuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class GUIApp extends JFrame {
 
+	private static Service1Grpc.Service1BlockingStub blockingStub;
+	private static Service1Grpc.Service1BlockingStub asyncStub;
+
+	private ServiceInfo serviceInfo;
+
+	private JFrame frame;
+
 	private JPanel contentPane;
+	private JTextField tFWidth;
+	private JTextField tFLength;
+	private JTextField tFHeigth;
+
+	private JTextArea textResponse;
+
+
 
 	/**
 	 * Launch the application.
@@ -30,12 +59,155 @@ public class GUIApp extends JFrame {
 	 * Create the frame.
 	 */
 	public GUIApp() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		String math_service_type = "_maths._tcp.local.";
+		discoverMathService(math_service_type);
+
+		String host = serviceInfo.getHostAddresses()[0];
+		int port = serviceInfo.getPort();
+
+		ManagedChannel channel = ManagedChannelBuilder
+				.forAddress(host,port)
+				.usePlaintext()
+				.build();
+
+		blockingStub = Service1Grpc.newBlockingStub(channel);
+		//asyncStub = Service1Grpc.newStub(channel);
+
+		//frame = new JFrame();
+		this.setTitle("Greener App");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(250, 150, 500, 500);
+
+		this.setVisible(true);
+
+		initialize();
+
+	}
+
+	private void discoverMathService(String service_type){
+		try {
+			JmDNS jmDNS = JmDNS.create(InetAddress.getLocalHost());
+
+			jmDNS.addServiceListener(service_type, new ServiceListener() {
+				@Override
+				public void serviceAdded(ServiceEvent event) {
+					System.out.println("Math Service added: " + event.getInfo());
+				}
+
+				@Override
+				public void serviceRemoved(ServiceEvent event) {
+					System.out.println("Math Service removed: " + event.getInfo());
+
+				}
+
+				@Override
+				public void serviceResolved(ServiceEvent event) {
+					System.out.println("Math Service resolved: "+event.getInfo());
+					serviceInfo = event.getInfo();
+
+					int port = serviceInfo.getPort();
+
+					System.out.println("resolving " + service_type + " with properties ...");
+					System.out.println("\t port: "+port);
+					System.out.println("\t type:"+event.getType());
+					System.out.println("\t description/properties: " + serviceInfo.getNiceTextString());
+					System.out.println("\t host: " +serviceInfo.getHostAddresses()[0]);
+
+				}
+			});
+			Thread.sleep(2000);
+			jmDNS.close();
+		}
+		catch (UnknownHostException e){
+			System.out.println(e.getMessage());
+		}
+		catch (IOException e){
+			System.out.println(e.getMessage());
+		}
+		catch (InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+
+
+	private void initialize(){
+
+
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
 
+		JLabel lblNewLabel = new JLabel("Service 1 - Heat Calculator (Unary)");
+		lblNewLabel.setFont(new Font("Arial Black", Font.PLAIN, 14));
+		lblNewLabel.setBounds(89, 0, 368, 21);
+		contentPane.add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Room Dimensions (meters)");
+		lblNewLabel_1.setBounds(10, 31, 306, 13);
+		contentPane.add(lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("Width");
+		lblNewLabel_2.setBounds(10, 57, 45, 13);
+		contentPane.add(lblNewLabel_2);
+
+		tFWidth = new JTextField();
+		tFWidth.setBounds(43, 54, 82, 19);
+		contentPane.add(tFWidth);
+		tFWidth.setColumns(10);
+
+		JLabel lblNewLabel_3 = new JLabel("Length");
+		lblNewLabel_3.setBounds(135, 57, 52, 13);
+		contentPane.add(lblNewLabel_3);
+
+		tFLength = new JTextField();
+		tFLength.setBounds(190, 54, 96, 19);
+		contentPane.add(tFLength);
+		tFLength.setColumns(10);
+
+		JLabel lblNewLabel_4 = new JLabel("Height");
+		lblNewLabel_4.setBounds(296, 57, 52, 13);
+		contentPane.add(lblNewLabel_4);
+
+		tFHeigth = new JTextField();
+		tFHeigth.setBounds(349, 54, 96, 19);
+		contentPane.add(tFHeigth);
+		tFHeigth.setColumns(10);
+
+		JButton btnNewButton = new JButton("Calculate");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				double width = Double.parseDouble(tFWidth.getText());
+				double length = Double.parseDouble(tFLength.getText());
+				double heigth = Double.parseDouble(tFHeigth.getText());
+
+				Service1OuterClass.HeatOutputRequest req = Service1OuterClass.HeatOutputRequest.newBuilder()
+						.setHeightRoom(heigth).setLengthRoom(length).setWidthRoom(width)
+						.build();
+
+				Service1OuterClass.HeatOutput response = blockingStub.getHeatOutput(req);
+
+				textResponse.append("BTU: "+response.getBTU()+"or kW: "+response.getKW()+"\n");
+
+				System.out.println("BTU: "+ response.getBTU()+"or kW: "+response.getKW());
+				
+			}
+		});
+		btnNewButton.setBounds(43, 99, 85, 21);
+		contentPane.add(btnNewButton);
+
+		textResponse = new JTextArea();
+		textResponse.setWrapStyleWord(true);
+		textResponse.setRows(3);
+		textResponse.setLineWrap(true);
+		textResponse.setColumns(20);
+		textResponse.setBounds(151, 80, 294, 57);
+
+		JScrollPane scrollPane = new JScrollPane(textResponse);
+		scrollPane.setBounds(151, 80, 294, 57);
+
+		contentPane.add(scrollPane);
+	}
 }
